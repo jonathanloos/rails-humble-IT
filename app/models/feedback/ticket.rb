0a24d7adf42710a7504287afb91d14c6d8c9dfd8
Jonathan Loos <jonathanloos97@gmail.com>
2020-03-31 13:58:13 -0400

    It's.... working

--- app/models/feedback/ticket.rb
+++ app/models/feedback/ticket.rb
@@ -1,7 +1,30 @@
 module Feedback
   class Ticket < ApplicationRecord
-    has_many :comments, class_name: "Feedback::Comment"
+    has_many :comments
 
     belongs_to :user, optional: true
+
+    enum status: {
+        pending: 0,
+        in_progress: 1,
+        closed: 2,
+        need_more_info: 3,
+    }
+
+    enum category: {
+        uncategorized: 0,
+        bug: 1,
+        feature: 2,
+        question: 3,
+        support: 4,
+    }
+
+    enum priority: {
+        low: 1,
+        medium: 2,
+        high: 3,
+        urgent: 4,
+    }
+
   end
 end
