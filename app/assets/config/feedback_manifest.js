3e818475b86b62266f697fa61d724141e2c9fb17
Jonathan Loos <jonathanloos97@gmail.com>
2020-04-14 14:40:02 -0400

    Beginnings of a design overhaul

--- app/assets/config/feedback_manifest.js
+++ app/assets/config/feedback_manifest.js
@@ -1,1 +1,2 @@
 //= link_directory ../stylesheets/feedback .css
+//= link feedback/undraw_collaborators_prrw.svg
--- lib/feedback/engine.rb
+++ lib/feedback/engine.rb
@@ -3,8 +3,8 @@ module Feedback
     isolate_namespace Feedback
 
     initializer "feedback", before: :load_config_initializers do |app|
-      app.config.assets.precompile << "feedback/application.scss"
-      app.config.assets.precompile << "feedback/feedbackFilter_controller.js"
+      app.config.assets.precompile << "feedback/ticket.scss"
+      app.config.assets.precompile << "feedback/undraw_collaborators_prrw.svg"
 
       config.paths["db/migrate"].expanded.each do |expanded_path|
         Rails.application.config.paths["db/migrate"] << expanded_path
